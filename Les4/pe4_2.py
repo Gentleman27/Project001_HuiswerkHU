e013aeb04fbf19655f191dfff57db09b2b00c0ea
Gentleman27 <32332886+Gentleman27@users.noreply.github.com>
2018-09-17 15:49:50 +0200

    Third commit, extra regel commentaar

--- Les4/pe4_2.py
+++ Les4/pe4_2.py
@@ -1,4 +1,4 @@
 uurloon = float(input("Wat verdien je per uur: "))
 aantalUur = float(input("Hoeveel uur heb je gewerkt: "))
 print(str(aantalUur) + " uur werken levert €" + str(uurloon*aantalUur) + " op." )
-aanpassing = "doe het voor de git"
+aanpassing = "doe het voor das git"
